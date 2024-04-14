3650b44e2b3adc6480e7574030bdba66259e8acf
BAUu <yimej07@gmail.com>
2024-04-14 17:42:33 +0900

    feat: sensor_plus packege 설치
    수평계 만들기
    화면에 수평 시각화

--- lib/tilt_sensor.dart
+++ lib/tilt_sensor.dart
@@ -1,33 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:sensors_plus/sensors_plus.dart';
 
 class SensorApp extends StatelessWidget {
   const SensorApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final centerX = MediaQuery.of(context).size.width / 2 -50;
-    final centerY = MediaQuery.of(context).size.height / 2 -50;
+    //가로 모드
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ]);
+    final centerX = MediaQuery.of(context).size.width / 2 - 50;
+    final centerY = MediaQuery.of(context).size.height / 2 - 50;
     return Scaffold(
       body: Stack(
         children: [
-          Positioned(
-            left: centerX,
-            top: centerY,
-            child: Container(
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                // color: Colors.blue,
+          StreamBuilder<AccelerometerEvent>(
+            stream: accelerometerEventStream(),
+            builder: (context, snapshot) {
+              if(!snapshot.hasData){
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
 
-              ),
-            width: 100,
-            height: 100,
-              color: Colors.orangeAccent,
+              final event = snapshot.data!;
+              List<double> accelerometerValues =[event.x, event.y, event.z];
+              print(accelerometerValues);
 
-          ),),
-
+              return Positioned(
+                left: centerX+ event.y*15,
+                top: centerY + event.x*15,
+                child: Container(
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.blue,
+                  ),
+                  width: 100,
+                  height: 100,
+                ),
+              );
+            }
+          ),
         ],
       ),
-
     );
   }
 }
